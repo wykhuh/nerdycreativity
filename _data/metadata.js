c72511ac4fcba584227ebacfdaa2a9babdcd8982
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-06-21 05:40:17 -0700

    change tab to spaces

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
-	title: "Nerdy Creativity",
-	url: "https://nerdycreativity.net/",
-	language: "en",
-	description: "Website for me, a nerd who likes to create things.",
+  title: "Nerdy Creativity",
+  url: "https://nerdycreativity.net/",
+  language: "en",
+  description: "Website for me, a nerd who likes to create things.",
 };
